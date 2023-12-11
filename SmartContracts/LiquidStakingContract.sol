9c283080804af34bdcb4a0d291d1841815437a1a
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2023-12-11 00:58:18 -0800

    Update LiquidStakingContract.sol

--- SmartContracts/LiquidStakingContract.sol
+++ SmartContracts/LiquidStakingContract.sol
@@ -12,10 +12,15 @@ interface ICustomToken {
 contract LiquidStakingContract is ERC20Upgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeable {
     ICustomToken public dddToken;
     mapping(address => bool) public whitelistedTokens;
-    mapping(address => uint256) public stakingBalances;
-    mapping(address => uint256) public rewardBalances;
+    struct Stake {
+        uint256 amount;
+        uint256 startTime;
+        uint256 lockPeriod;
+    }
+    mapping(address => Stake) public stakingBalances;
+    uint256 public totalStaked;
 
-    event Staked(address indexed user, address token, uint256 amount);
+    event Staked(address indexed user, address token, uint256 amount, uint256 lockPeriod);
     event Unstaked(address indexed user, address token, uint256 amount);
     event RewardsDistributed(uint256 totalRewards);
 
@@ -35,52 +40,54 @@ contract LiquidStakingContract is ERC20Upgradeable, ReentrancyGuardUpgradeable, 
         whitelistedTokens[token] = false;
     }
 
-    function stake(address token, uint256 amount) external nonReentrant {
+    function stake(address token, uint256 amount, uint256 lockPeriod) external nonReentrant {
         require(whitelistedTokens[token], "Token not whitelisted");
         require(amount > 0, "Cannot stake 0");
+        require(lockPeriod == 3 || lockPeriod == 6 || lockPeriod == 12 || lockPeriod == 24 || lockPeriod == 60, "Invalid lock period");
 
         ERC20Upgradeable(token).transferFrom(msg.sender, address(this), amount);
-        stakingBalances[msg.sender] += amount;
 
-        // Mint sTokens to represent the staked amount
-        _mint(msg.sender, amount);
+        stakingBalances[msg.sender] = Stake(amount, block.timestamp, lockPeriod);
+        totalStaked += amount;
 
-        emit Staked(msg.sender, token, amount);
+        _mint(msg.sender, amount);  // Mint sTokens to represent the staked amount
+
+        emit Staked(msg.sender, token, amount, lockPeriod);
     }
 
-    function unstake(address token, uint256 amount) external nonReentrant {
-        require(amount > 0, "Cannot unstake 0");
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+    function unstake(address token) external nonReentrant {
+        uint256 amount = stakingBalances[msg.sender].amount;
+        require(amount > 0, "No tokens staked");
+        require(block.timestamp >= stakingBalances[msg.sender].startTime + (stakingBalances[msg.sender].lockPeriod * 30 days), "Stake is still locked");
 
-        stakingBalances[msg.sender] -= amount;
-        rewardBalances[msg.sender] += calculateRewards(msg.sender);
+        totalStaked -= amount;
 
-        // Burn sTokens representing the unstaked amount
-        _burn(msg.sender, amount);
+        _burn(msg.sender, amount);  // Burn sTokens representing the unstaked amount
 
         ERC20Upgradeable(token).transfer(msg.sender, amount);
         emit Unstaked(msg.sender, token, amount);
-    }
 
-    function calculateRewards(address staker) internal returns (uint256) {
-        // Implement your reward calculation logic here
-        // This function should update `rewardBalances[staker]` and return the calculated reward
-        uint256 rewards = ...; // Calculated rewards
-        rewardBalances[staker] += rewards;
-        return rewards;
-    }
-
-    function claimRewards() external nonReentrant {
-        uint256 rewards = rewardBalances[msg.sender];
-        require(rewards > 0, "No rewards to claim");
-
-        dddToken.mint(msg.sender, rewards);
-        rewardBalances[msg.sender] = 0;
+        delete stakingBalances[msg.sender];
     }
 
     function distributeRewards() external onlyOwner {
-        // Implement the logic to distribute rewards to all stakers
-        // This might involve iterating over all stakers and calling `calculateRewards`
-        emit RewardsDistributed(...); // Total distributed rewards
+        uint256 totalRewards = 0;
+        for(uint256 i = 0; i < stakers.length; i++) {
+            address staker = stakers[i];
+            Stake memory userStake = stakingBalances[staker];
+            uint256 rewardRate = getRewardRate(userStake.lockPeriod);
+            uint256 reward = (userStake.amount * rewardRate * (block.timestamp - userStake.startTime)) / (365 days) / 100;
+            totalRewards += reward;
+            dddToken.mint(staker, reward);
+        }
+        emit RewardsDistributed(totalRewards);
+    }
+
+    function getRewardRate(uint256 lockPeriod) public pure returns (uint256) {
+        if (lockPeriod == 3) return 25;
+        if (lockPeriod == 6) return 50;
+        if (lockPeriod == 12) return 75;
+        if (lockPeriod == 24) return 100;
+        return 125;  // For 60 months
     }
 }
