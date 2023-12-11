d1c3837cef429191132eed1904cdd7dbad9c16f7
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2023-12-11 02:38:10 -0800

    Update RedemptionContract.sol

--- SmartContracts/RedemptionContract.sol
+++ SmartContracts/RedemptionContract.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
@@ -9,18 +10,22 @@ interface IStakingContract {
     function totalStaked() external view returns (uint256);
 }
 
-contract RedemptionContract is Initializable, AccessControlUpgradeable {
+contract RedemptionContract is Initializable, AccessControlUpgradeable, ReentrancyGuardUpgradeable {
     IERC20Upgradeable public sToken;
     IStakingContract public stakingContract;
 
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+    mapping(address => bool) public whitelistedAddresses;
 
     event Redeemed(address indexed user, uint256 sTokenAmount, uint256 redeemedAmount);
     event FundsAdded(address indexed from, uint256 amount);
     event FundsWithdrawn(address indexed to, uint256 amount);
+    event sTokensWithdrawn(address indexed to, uint256 amount);
+    event AddressWhitelisted(address indexed addressWhitelisted);
 
     function initialize(address _sToken, address _stakingContract, address multisigAddress) public initializer {
         __AccessControl_init();
+        __ReentrancyGuard_init();
 
         sToken = IERC20Upgradeable(_sToken);
         stakingContract = IStakingContract(_stakingContract);
@@ -29,7 +34,7 @@ contract RedemptionContract is Initializable, AccessControlUpgradeable {
         _setupRole(MANAGER_ROLE, multisigAddress);
     }
 
-    function redeem(uint256 sTokenAmount) external {
+    function redeem(uint256 sTokenAmount) external nonReentrant {
         require(sTokenAmount > 0, "Cannot redeem 0 tokens");
         
         uint256 totalStakedFunds = stakingContract.totalStaked();
@@ -37,7 +42,6 @@ contract RedemptionContract is Initializable, AccessControlUpgradeable {
         require(totalSTokens > 0, "No sTokens in circulation");
 
         uint256 redeemableAmount = (totalStakedFunds * sTokenAmount) / totalSTokens;
-        require(redeemableAmount > 0, "Redeemable amount is 0");
         require(redeemableAmount <= address(this).balance, "Insufficient funds in contract");
 
         sToken.transferFrom(msg.sender, address(this), sTokenAmount);
@@ -47,23 +51,24 @@ contract RedemptionContract is Initializable, AccessControlUpgradeable {
     }
 
     function withdrawFunds(address to, uint256 amount) external onlyRole(MANAGER_ROLE) {
-        require(amount > 0, "Cannot withdraw 0");
         require(amount <= address(this).balance, "Insufficient funds in contract");
-
         payable(to).transfer(amount);
         emit FundsWithdrawn(to, amount);
+    }
+
+    function withdrawSTokens(address to, uint256 amount) external onlyRole(MANAGER_ROLE) {
+        require(whitelistedAddresses[to], "Address not whitelisted");
+        require(amount <= sToken.balanceOf(address(this)), "Insufficient sTokens in contract");
+        sToken.transfer(to, amount);
+        emit sTokensWithdrawn(to, amount);
+    }
+
+    function whitelistAddress(address addr) external onlyRole(MANAGER_ROLE) {
+        whitelistedAddresses[addr] = true;
+        emit AddressWhitelisted(addr);
     }
 
     function addFunds() external payable onlyRole(MANAGER_ROLE) {
         emit FundsAdded(msg.sender, msg.value);
     }
-
-    function grantManagerRole(address newManager) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        grantRole(MANAGER_ROLE, newManager);
-    }
-
-    function revokeManagerRole(address manager) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        revokeRole(MANAGER_ROLE, manager);
-    }
-
 }
