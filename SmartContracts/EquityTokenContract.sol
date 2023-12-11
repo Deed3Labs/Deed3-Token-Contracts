5e3eb8c4582e3a2f178c13470754ed0fee9a6480
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2023-12-11 01:05:49 -0800

    Update EquityTokenContract.sol

--- SmartContracts/EquityTokenContract.sol
+++ SmartContracts/EquityTokenContract.sol
@@ -6,28 +6,28 @@ import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol"
 
 /**
  * @title The Deed3 Equity Token (DDD)
- * @dev This contract implements an upgradable ERC20 token with ownership and roles managed via AccessControl.
+ * @dev Implements an upgradable ERC20 token with role-based access control.
  */
-contract TheDeed3EquityToken is Initializable, ERC20Upgradeable, AccessControlUpgradeable {
-    // Define a role for minters
+contract TheDeed3EquityToken is ERC20Upgradeable, AccessControlUpgradeable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     /**
      * @dev Initializes the contract with token details and sets up roles.
+     * @param multisigWalletAddress The address that will have the default admin role.
      */
     function initialize(address multisigWalletAddress) public initializer {
         __ERC20_init("The Deed3 Equity Token", "DDD");
         __AccessControl_init();
 
-        // Set up the default admin role to the multisig wallet
+        // Set up the default admin role
         _setupRole(DEFAULT_ADMIN_ROLE, multisigWalletAddress);
 
-        // Optionally, you can also grant the MINTER_ROLE to the multisig wallet initially
+        // Grant MINTER_ROLE to the multisig wallet initially
         _setupRole(MINTER_ROLE, multisigWalletAddress);
     }
 
     /**
-     * @dev Allows users with the MINTER_ROLE to mint new tokens.
+     * @dev Mints new tokens. Access restricted to addresses with the MINTER_ROLE.
      * @param to The address that will receive the minted tokens.
      * @param amount The amount of tokens to mint.
      */
@@ -39,8 +39,7 @@ contract TheDeed3EquityToken is Initializable, ERC20Upgradeable, AccessControlUp
     }
 
     /**
-     * @dev Override of AccessControl's renounceRole to prevent renouncing the DEFAULT_ADMIN_ROLE.
-     * This ensures that the default admin role cannot be renounced accidentally.
+     * @dev Prevents renouncing of the DEFAULT_ADMIN_ROLE to ensure continued control over the contract.
      * @param role The role to be renounced.
      * @param account The account renouncing its role.
      */
@@ -48,4 +47,5 @@ contract TheDeed3EquityToken is Initializable, ERC20Upgradeable, AccessControlUp
         require(role != DEFAULT_ADMIN_ROLE, "Cannot renounce the default admin role");
         super.renounceRole(role, account);
     }
+
 }
